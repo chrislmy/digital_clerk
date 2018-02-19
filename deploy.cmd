33a24d2a386aec9f3c87f8d8564c562a64c2b4ae
Christopher Lau Min Yi <zcabmyl@ucl.ac.uk>
2018-02-19 11:40:47 +0000

    emit .exe extension

--- deploy.cmd
+++ deploy.cmd
@@ -119,7 +119,7 @@ IF NOT EXIST "%DEPLOYMENT_TARGET%\env\azure.env.%PYTHON_RUNTIME%.txt" (
 
 :: 4. Install packages
 echo Pip install requirements.
-python361x64\python -m pip install --upgrade -r D:\home\site\wwwroot\requirements.txt
+python361x64\python.exe -m pip install --upgrade -r D:\home\site\wwwroot\requirements.txt
 IF !ERRORLEVEL! NEQ 0 goto error
 
 REM Add additional package installation here
