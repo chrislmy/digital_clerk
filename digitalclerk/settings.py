dadc0971a49664912bf69e3629500c283d74ff8f
Christopher Lau Min Yi <zcabmyl@ucl.ac.uk>
2018-02-24 15:49:13 +0000

    modified settings.py

--- digitalclerk/settings.py
+++ digitalclerk/settings.py
@@ -147,4 +147,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/var/www/html/static/"
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
